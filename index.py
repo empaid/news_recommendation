5667819956cfb19e7a6455bbbc398493e0d1e6af
empaid26 <empaid26@gmail.com>
2023-05-07 04:15:38 +0530

    Add functionality of pagination in the recommend route

--- index.py
+++ index.py
@@ -37,18 +37,41 @@ def update_news_list():
 update_thread = threading.Thread(target=update_news_list)
 update_thread.start()
 
-def recommend_news(watched_news_idx):
+def recommend_articles(watched_news_idx, page_num=1, page_size=20):
     item_scores = item_similarities[watched_news_idx,:].sum(axis=0)
     recommendations = item_scores.argsort()[::-1]
-    print(len(recommendations))
-    response = ""
-    for i, index in enumerate(recommendations):
+    total_results = len(recommendations)
+    
+    num_pages = (total_results + page_size - 1) // page_size
+    if page_num > num_pages:
+        return [], total_results
+    
+    start_idx = (page_num - 1) * page_size
+    end_idx = start_idx + page_size
+    page_recommendations = recommendations[start_idx:end_idx]
+
+    articles = []
+    for i, index in enumerate(page_recommendations):
         article = news_list[index]
-        response += f"{i + 1}. {article['category']}, {article['title']}\n"
+        json_article = {
+            'id': str(article['_id']),
+            'source': article['source'],
+            'author': article['author'],
+            'title': article['title'],
+            'description': article['description'],
+            'url': article['url'],
+            'urlToImage': article['urlToImage'],
+            'publishedAt': article['publishedAt'],
+            'content': article['content'],
+            'category': article['category'],
+            'country': article['country']
+        }
+        articles.append(json_article)
 
-    return response
+    return articles, total_results
 
-@app.route('/')
+
+@app.route('/recommend')
 def index():
     if 'userId' not in session or user_collection.find_one({'userId' : session.get('userId')}) is None:
         session['userId'] = str(uuid.uuid4())
@@ -66,7 +89,12 @@ def index():
     else:
         watched_news_idx = []
 
-    response = recommend_news(watched_news_idx)
+    # Get page and pageSize parameters from the request
+    page = int(request.args.get('page', 1))
+    pageSize = int(request.args.get('pageSize', 20))
+
+    articles, total_results = recommend_articles(watched_news_idx, page, pageSize)
+    response = { "status": "ok", "totalResults": total_results, "articles": articles}
 
     return response
 
